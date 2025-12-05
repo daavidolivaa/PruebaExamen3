b6b8198da8165f77bf9cf68e024f4808ad3ac6b9
daavidolivaa <deividolivahuelamo@gmail.com>
2025-12-05 17:05:42 +0100

    J67IK6R

---
+++ app/src/main/java/es/etg/pdmd/pruebaexamen3/MainActivity.kt
@@ -0,0 +1,23 @@
+package es.etg.pdmd.pruebaexamen3
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        const val EXTRA_NOMBRE = "nombre"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
